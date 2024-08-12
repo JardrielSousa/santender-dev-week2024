ac8c059f2c261642da62080ea091768c172437f9
Your Name <jardriel.redes18@gmail.com>
2024-08-12 19:23:57 -0300

    first commit

---
+++ src/test/java/com/dio/DevSantanderWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSantanderWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
